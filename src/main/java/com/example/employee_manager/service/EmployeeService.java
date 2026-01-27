41d7e15930904a508ad3402f84e33c2f6d1f524b
NK0807 <hyue3434@gmail.com>
2026-01-28 05:13:47 +0900

    Entity, Repository, Serviceを作成

---
+++ src/main/java/com/example/employee_manager/service/EmployeeService.java
@@ -0,0 +1,37 @@
+package com.example.employee_manager.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.employee_manager.entity.Employee;
+import com.example.employee_manager.repository.EmployeeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+	
+	private final EmployeeRepository repository;
+	
+	// 全体取得
+	public List<Employee> findAll() {
+		return repository.findAll();
+	}
+	
+	// 保存 (新規・更新)
+	public void save(Employee employee) {
+		repository.save(employee);
+	}
+	
+	// 1件取得
+	public Employee get(Integer id) {
+		return repository.findById(id).orElse(null);
+	}
+	
+	// 削除
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+}
